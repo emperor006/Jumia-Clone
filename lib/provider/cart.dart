b3e67073951602e5e0860abb3a5b644b42438750
emperor006 <mauricecharles1000@gmail.com>
2022-09-15 16:53:46 +0100

    used multiprovider

---
+++ lib/provider/cart.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+class CartItem {
+  final String id, title;
+  final double price;
+  final int quantity;
+  CartItem(
+      {required this.id,
+      required this.price,
+      required this.quantity,
+      required this.title});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItems(String id, double price, String title) {
+    if (_items.containsKey(id)) {
+      _items.update(
+          id,
+          (value) => CartItem(
+              id: value.id,
+              price: value.price,
+              quantity: value.quantity + 1,
+              title: value.title));
+    } else {
+      _items.putIfAbsent(
+          id,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              price: price,
+              quantity: 1,
+              title: title));
+    }
+  }
+}
